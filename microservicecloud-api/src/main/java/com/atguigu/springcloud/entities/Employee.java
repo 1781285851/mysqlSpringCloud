fc345659bfa5aaee443f9d4fb98d314b3839b5f5
zhangmn <Zhang1880!>
2019-11-27 17:06:27 +0800

    给实体类人添加注释

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Employee.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Employee.java
@@ -10,13 +10,22 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee implements Serializable {
+    //主键id
     private Integer id;
+    //用户编码
     private String code;
+    //用户姓名
     private String qname;
+    //性别 1：男 0：女
     private Integer gender;
+    //密码
     private String password;
+    //电话
     private String phone;
+    //备注
     private String description;
+    //权限 1：用户 0：管理员
     private Integer auth;
+    //状态 1：正常 0：删除
     private Integer status;
 }
