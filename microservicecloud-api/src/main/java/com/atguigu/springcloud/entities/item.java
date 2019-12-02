3f5a44c190cb6bfb20b52a2bcc158331bd62678b
cds <cds123@qq.com>
2019-12-02 14:48:26 +0800

    修改item实体类

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/item.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/item.java
@@ -1,8 +1,14 @@
 package com.atguigu.springcloud.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.Date;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class item implements Serializable {
     //id
     private Integer id;
@@ -16,6 +22,6 @@ public class item implements Serializable {
     private Date createDate;
     //更新时间
     private Date updateDate;
-    //项目状态
+    //项目状态(1:正常，0：删除)
     private Integer isDelete;
 }
